813cf1c90d1d625ab99bf9507eed9a844f39ad1b
mathew <mathewsibi06@gmail.com>
2025-06-20 14:44:27 +0530

    calculator

--- matgew.py
+++ matgew.py
@@ -1,5 +1,14 @@
-a=int(input("enter a number"))
-if(a%2==0):
-    print("the number is even")
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("1:addition, 2:substration, 3:multiplication, 4:division")      
+choice = int(input("enter a number"))      
+if(choice == 1):
+    print(a+b)
+if(choice == 2):
+    print(a-b)
+if(choice == 3):
+    print(a*b)
+if(choice == 4):
+    print(a/b)
 else:
-    print("the number is odd")    
+    print("invalid choice")
